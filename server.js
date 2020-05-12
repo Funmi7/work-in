a9ea91b0c82f55426b2965bf626008696388dfe1
funmi7 <talabifunmilayo@gmail.com>
2020-05-12 15:35:28 +0100

    Connected mongodb atlas and added more dependencies

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require('dotenv/config');
 const server = express();
-server.get('/', (req, res) => {
-  res.status(200).json('Api is working')
+
+server.use(cors());
+server.use(bodyParser.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json("Api is working");
 });
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => {
+    console.log("Connected to DB!");
+  }
+);
 
 module.exports = server;
