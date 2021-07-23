a923def655302bab13b2524fcc68e153cdd2fcf6
funtee7 <talabifunmilayo@gmail.com>
2021-07-23 22:30:09 +0100

    reconfigured database

--- server.js
+++ server.js
@@ -2,34 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-require('dotenv/config');
+require("dotenv/config");
 const server = express();
 
-const authRouter = require('./auth/auth-router');
-const imageRouter = require('./routes/image-routers');
-const userRouter = require('./routes/users');
+const authRouter = require("./auth/auth-router");
+const imageRouter = require("./routes/image-routers");
+const userRouter = require("./routes/users");
 
 server.use(cors());
 server.use(bodyParser.json());
-server.use(express.json())
+server.use(express.json());
 
-server.use('/api/auth', authRouter)
-server.use('/images', imageRouter)
-server.use('/users', userRouter)
+server.use("/api/auth", authRouter);
+server.use("/images", imageRouter);
+server.use("/users", userRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json("Api is working");
 });
 
-
-
 mongoose.connect(
   process.env.MONGODB_URL,
-  { useNewUrlParser: true },
+  { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("Connected to DB!");
   }
 );
-
 
 module.exports = server;
